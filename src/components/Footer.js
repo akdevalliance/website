fa9441628fe0e769c801a6a73d589a764db28da8
Conroy Whitney <conroy.whitney@gmail.com>
2019-10-12 19:25:30 -0800

    Remove broken/unused links in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,27 +39,12 @@ const Footer = class extends React.Component {
                         Form Examples
                       </Link>
                     </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
                   </ul>
                 </section>
               </div>
               <div className="column is-4">
                 <section>
                   <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
                     <li>
                       <Link className="navbar-item" to="/contact">
                         Contact
