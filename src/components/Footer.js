3c22e5dc34cbb3821a936a93c685cbe8a552695f
Amy Carney <click2carney@yahoo.com>
2019-10-13 09:02:03 -0800

    update alt text for icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Codi the Yeti"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
